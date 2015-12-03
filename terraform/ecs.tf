a484e0e67d8900c52048e055cac84ef1f7c397fa
Ron Kurr <kurr@kurron.org>
2015-12-03 20:08:49 +0000

    Created a task definition

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,3 +1,13 @@
 resource "aws_ecs_cluster" "example" {
     name = "example-cluster"
 }
+
+resource "aws_ecs_task_definition" "jenkins" {
+  family = "sample-app"
+  container_definitions = "${file("task-definitions/simple-app.json")}"
+
+  volume {
+    name = "my-vol"
+    host_path = "/ecs/sample-app"
+  }
+}
